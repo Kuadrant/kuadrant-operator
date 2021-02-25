967b028498dd3548501ebd72186af680ee40287f
Joaquim Moreno <jmprusi@keepalive.io>
2021-02-25 17:08:32 +0100

    adds echo-api and deploys it to the local devel environment

--- utils/local-deployment/local-setup.sh
+++ utils/local-deployment/local-setup.sh
@@ -39,6 +39,9 @@ echo "Deploying Kuadrant control plane"
 kustomize build config/default | kubectl -n "${KUADRANT_NAMESPACE}" apply -f -
 kubectl -n "${KUADRANT_NAMESPACE}" patch deployment kuadrant-controller-manager -p '{"spec": {"template": {"spec":{"containers":[{"name": "manager","image":"kuadrant:devel", "imagePullPolicy":"IfNotPresent"}]}}}}'
 
+echo "Deploying EchoAPI to the default namespace"
+kubectl apply -n default -f utils/local-deployment/echo-api.yaml
+
 echo "Wait for all deployments to be up"
 kubectl -n "${KUADRANT_NAMESPACE}" wait --timeout=300s --for=condition=Available deployments --all
 

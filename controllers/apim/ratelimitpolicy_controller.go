fa680c20400864e733c2add6183179746d042c35
Rahul Anand <rahulanand16nov@gmail.com>
2022-02-17 15:38:46 +0530

    add ratelimit per-route config to include vh-level actions (#92)

--- controllers/apim/ratelimitpolicy_controller.go
+++ controllers/apim/ratelimitpolicy_controller.go
@@ -439,6 +439,12 @@ func routeRateLimitsPatch(vHostName string, routeName string, rateLimits []*apim
 		"operation": "MERGE",
 		"value": map[string]interface{}{
 			"route": map[string]interface{}{
+				"typed_per_filter_config": map[string]interface{}{
+					"envoy.filters.http.ratelimit": map[string]interface{}{
+						"@type":          "type.googleapis.com/envoy.extensions.filters.http.ratelimit.v3.RateLimitPerRoute",
+						"vh_rate_limits": "include",
+					},
+				},
 				"rate_limits": common.EnvoyFilterRatelimitsUnstructured(rateLimits),
 			},
 		},

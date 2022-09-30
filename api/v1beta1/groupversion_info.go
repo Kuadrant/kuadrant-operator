c8713a87a2d18cae11b7e6cd56af96d7a774d9ae
dd di cesare <didi@posteo.net>
2022-09-30 18:16:02 +0200

    [refactor, api] Adding controllers APIs and merging pkg/common

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1beta1 contains API Schema definitions for the kuadrant v1beta1 API group
-//+kubebuilder:object:generate=true
-//+groupName=kuadrant.kuadrant.io
+// +kubebuilder:object:generate=true
+// +groupName=kuadrant.kuadrant.io
 package v1beta1
 
 import (

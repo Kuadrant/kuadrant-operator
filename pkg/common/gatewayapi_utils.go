76cf69bfc218d39d89331a652203f5813dafd08d
dd di cesare <didi@posteo.net>
2023-01-27 18:44:35 +0100

    [utils] Refactor: new TargetHostnames and ValidateHierarchicalRules
    * There was no need to pass ctx and have them repeated in the
      controllers

--- pkg/common/gatewayapi_utils.go
+++ pkg/common/gatewayapi_utils.go
@@ -384,6 +384,49 @@ func (g GatewayWrapper) Hostnames() []string {
 	return hostnames
 }
 
+// TargetHostnames returns an array of hostnames coming from the network object (HTTPRoute, Gateway)
+func TargetHostnames(targetNetworkObject client.Object) ([]string, error) {
+	hosts := make([]string, 0)
+	switch obj := targetNetworkObject.(type) {
+	case *gatewayapiv1alpha2.HTTPRoute:
+		for _, hostname := range obj.Spec.Hostnames {
+			hosts = append(hosts, string(hostname))
+		}
+	case *gatewayapiv1alpha2.Gateway:
+		for idx := range obj.Spec.Listeners {
+			if obj.Spec.Listeners[idx].Hostname != nil {
+				hosts = append(hosts, string(*obj.Spec.Listeners[idx].Hostname))
+			}
+		}
+	}
+
+	if len(hosts) == 0 {
+		hosts = append(hosts, "*")
+	}
+
+	return hosts, nil
+}
+
+// ValidateHierarchicalRules returns error if the policy rules hostnames fail to match the target network hosts
+func ValidateHierarchicalRules(policy KuadrantPolicy, targetNetworkObject client.Object) error {
+	targetHostnames, err := TargetHostnames(targetNetworkObject)
+	if err != nil {
+		return err
+	}
+
+	if valid, invalidHost := ValidSubdomains(targetHostnames, policy.GetRulesHostnames()); !valid {
+		return fmt.Errorf(
+			"rule host (%s) does not follow any hierarchical constraints, "+
+				"for the %T to be validated, it must match with at least one of the target network hostnames %+q",
+			invalidHost,
+			policy,
+			targetHostnames,
+		)
+	}
+
+	return nil
+}
+
 func GetGatewayWorkloadSelector(ctx context.Context, cli client.Client, gateway *gatewayapiv1alpha2.Gateway) (map[string]string, error) {
 	address, found := Find(
 		gateway.Status.Addresses,

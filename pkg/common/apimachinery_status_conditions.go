64b34c79d94d395aed9108542b5e46bfc44ba6d6
Artem Tiupin <artem.tiupin@gmail.com>
2023-04-26 15:00:11 +0100

    refactor: optimize CopyConditions and reuse it in ConditionMarshal
    - Refactored CopyConditions function to avoid unnecessary appends and allocate slice with exact length
    - Reused CopyConditions in ConditionMarshal instead of using append for performance improvement and null safety

--- pkg/common/apimachinery_status_conditions.go
+++ pkg/common/apimachinery_status_conditions.go
@@ -9,19 +9,17 @@ import (
 
 // CopyConditions copies the set of conditions
 func CopyConditions(conditions []metav1.Condition) []metav1.Condition {
-	newConditions := make([]metav1.Condition, 0)
+	newConditions := make([]metav1.Condition, len(conditions))
 	for idx := range conditions {
 		// copy
-		newCondition := conditions[idx]
-		newConditions = append(newConditions, newCondition)
+		newConditions[idx] = conditions[idx]
 	}
 	return newConditions
 }
 
 // ConditionMarshal marshals the set of conditions as a JSON array, sorted by condition type.
 func ConditionMarshal(conditions []metav1.Condition) ([]byte, error) {
-	var condCopy []metav1.Condition
-	condCopy = append(condCopy, conditions...)
+	condCopy := CopyConditions(conditions)
 	sort.Slice(condCopy, func(a, b int) bool {
 		return condCopy[a].Type < condCopy[b].Type
 	})

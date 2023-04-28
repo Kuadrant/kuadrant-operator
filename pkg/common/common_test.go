d4ca6b45bab3b6985971c72bab37eb78716f4b81
Artem Tiupin <artem.tiupin@gmail.com>
2023-04-28 18:45:32 +0100

    test: Add unit-tests for NamespacedNameToObjectKey (#167)
    + "sigs.k8s.io/controller-runtime/pkg/client" added as an import

--- pkg/common/common_test.go
+++ pkg/common/common_test.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"reflect"
 	"testing"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestValidSubdomains(t *testing.T) {
@@ -215,6 +217,44 @@ func TestGetEmptySliceIfNil(t *testing.T) {
 
 		result := GetEmptySliceIfNil(value)
 
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected %v, but got %v", expected, result)
+		}
+	})
+}
+
+func TestNamespacedNameToObjectKey(t *testing.T) {
+	t.Run("when a namespaced name is provided then return an ObjectKey with corresponding namespace and name", func(t *testing.T) {
+		namespacedName := "test-namespace/test-name"
+		defaultNamespace := "default"
+
+		result := NamespacedNameToObjectKey(namespacedName, defaultNamespace)
+
+		expected := client.ObjectKey{Name: "test-name", Namespace: "test-namespace"}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected %v, but got %v", expected, result)
+		}
+	})
+
+	t.Run("when only a name is provided then return an ObjectKey with the default namespace and provided name", func(t *testing.T) {
+		namespacedName := "test-name"
+		defaultNamespace := "default"
+
+		result := NamespacedNameToObjectKey(namespacedName, defaultNamespace)
+
+		expected := client.ObjectKey{Name: "test-name", Namespace: "default"}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected %v, but got %v", expected, result)
+		}
+	})
+
+	t.Run("when an empty string is provided, then return an ObjectKey with default namespace and empty name", func(t *testing.T) {
+		namespacedName := ""
+		defaultNamespace := "default"
+
+		result := NamespacedNameToObjectKey(namespacedName, defaultNamespace)
+
+		expected := client.ObjectKey{Name: "", Namespace: "default"}
 		if !reflect.DeepEqual(result, expected) {
 			t.Errorf("Expected %v, but got %v", expected, result)
 		}

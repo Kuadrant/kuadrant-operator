827f64c1d6e529e52cf37879cfa8a4aff4dadb59
Eguzki Astiz Lezaun <eastizle@redhat.com>
2023-09-28 17:28:43 +0200

    more tests

--- controllers/gateway_kuadrant_controller_test.go
+++ controllers/gateway_kuadrant_controller_test.go
@@ -9,10 +9,12 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	gatewayapiv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
+	kuadrantv1beta1 "github.com/kuadrant/kuadrant-operator/api/v1beta1"
 	"github.com/kuadrant/kuadrant-operator/pkg/common"
 )
 
@@ -62,6 +64,64 @@ var _ = Describe("Kuadrant Gateway controller", func() {
 					return false
 				}
 				return common.IsKuadrantManaged(existingGateway)
+			}, 15*time.Second, 5*time.Second).Should(BeTrue())
+		})
+	})
+
+	Context("Two kuadrant instances", func() {
+
+		BeforeEach(func() {
+			newKuadrantName := "second"
+			newKuadrant := &kuadrantv1beta1.Kuadrant{
+				TypeMeta:   metav1.TypeMeta{APIVersion: "v1beta1", Kind: "Kuadrant"},
+				ObjectMeta: metav1.ObjectMeta{Name: newKuadrantName, Namespace: testNamespace},
+			}
+			err := testClient().Create(context.Background(), newKuadrant)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(func() bool {
+				kuadrant := &kuadrantv1beta1.Kuadrant{}
+				err := k8sClient.Get(context.Background(), client.ObjectKey{Name: newKuadrantName, Namespace: testNamespace}, kuadrant)
+				if err != nil {
+					return false
+				}
+				if !meta.IsStatusConditionTrue(kuadrant.Status.Conditions, "Ready") {
+					return false
+				}
+				return true
+			}, time.Minute, 5*time.Second).Should(BeTrue())
+		})
+
+		It("new gateway should not be annotated", func() {
+			gateway := testBuildBasicGateway(gwName, testNamespace)
+			err := k8sClient.Create(context.Background(), gateway)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(func() bool {
+				existingGateway := &gatewayapiv1beta1.Gateway{}
+				err := k8sClient.Get(context.Background(), client.ObjectKeyFromObject(gateway), existingGateway)
+				if err != nil {
+					logf.Log.V(1).Info("[WARN] Getting gateway failed", "error", err)
+					return false
+				}
+
+				if meta.IsStatusConditionFalse(existingGateway.Status.Conditions, common.GatewayProgrammedConditionType) {
+					logf.Log.V(1).Info("[WARN] Gateway not ready")
+					return false
+				}
+
+				return true
+			}, 15*time.Second, 5*time.Second).Should(BeTrue())
+
+			// Check gateway is not annotated with kuadrant annotation
+			Eventually(func() bool {
+				existingGateway := &gatewayapiv1beta1.Gateway{}
+				err := k8sClient.Get(context.Background(), client.ObjectKeyFromObject(gateway), existingGateway)
+				if err != nil {
+					logf.Log.V(1).Info("[WARN] Getting gateway failed", "error", err)
+					return false
+				}
+				return !common.IsKuadrantManaged(existingGateway)
 			}, 15*time.Second, 5*time.Second).Should(BeTrue())
 		})
 	})

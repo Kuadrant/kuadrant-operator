495e49401c9871038e08b446d022b8702295abdb
Guilherme Cassolato <guicassolato@gmail.com>
2023-11-14 14:21:29 +0100

    fix the policy attachment type label in the ratelimitpolicy and authpolicy to 'direct'

--- api/v1beta2/ratelimitpolicy_types.go
+++ api/v1beta2/ratelimitpolicy_types.go
@@ -163,7 +163,7 @@ func (s *RateLimitPolicyStatus) Equals(other *RateLimitPolicyStatus, logger logr
 // RateLimitPolicy is the Schema for the ratelimitpolicies API
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:metadata:labels="gateway.networking.k8s.io/policy=inherited"
+// +kubebuilder:metadata:labels="gateway.networking.k8s.io/policy=direct"
 type RateLimitPolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`

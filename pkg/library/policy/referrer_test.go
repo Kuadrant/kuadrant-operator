2dde258b5cf1f0a8362f305e10721cbba6023627
KevFan <chfan@redhat.com>
2024-01-12 12:49:11 +0000

    refactor: referrer interface

---
+++ pkg/library/policy/referrer_test.go
@@ -0,0 +1,35 @@
+package policy
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/kuadrant/kuadrant-operator/pkg/library/utils"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestBackReferencesFromObject(t *testing.T) {
+	obj := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:   "gw-ns",
+			Name:        "gw-1",
+			Annotations: map[string]string{"kuadrant.io/testpolicies": `[{"Namespace":"app-ns","Name":"policy-1"},{"Namespace":"app-ns","Name":"policy-2"}]`},
+		},
+		Spec: corev1.ServiceSpec{},
+	}
+
+	policyKind := &utils.PolicyKindStub{}
+
+	refs := utils.Map(BackReferencesFromObject(obj, policyKind), func(ref client.ObjectKey) string { return ref.String() })
+	if !slices.Contains(refs, "app-ns/policy-1") {
+		t.Error("GatewayWrapper.PolicyRefs() should contain app-ns/policy-1")
+	}
+	if !slices.Contains(refs, "app-ns/policy-2") {
+		t.Error("GatewayWrapper.PolicyRefs() should contain app-ns/policy-2")
+	}
+	if len(refs) != 2 {
+		t.Fail()
+	}
+}
---
+++ pkg/library/utils/test_utils.go
@@ -0,0 +1,11 @@
+package utils
+
+type PolicyKindStub struct{}
+
+func (tpk *PolicyKindStub) Kind() string {
+	return "TestPolicy"
+}
+
+func (tpk *PolicyKindStub) BackReferenceAnnotationName() string {
+	return "kuadrant.io/testpolicies"
+}

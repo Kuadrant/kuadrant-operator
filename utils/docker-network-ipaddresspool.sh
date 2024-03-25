48f75bf8182fdcc1ff837f98c61141bc4b6d31cd
Jason Madigan <jason@jasonmadigan.com>
2024-03-25 11:45:10 +0000

    Podman fixes
    Use subnet inside /24 network
    
    remove dead line

--- utils/docker-network-ipaddresspool.sh
+++ utils/docker-network-ipaddresspool.sh
@@ -11,10 +11,16 @@ set -euo pipefail
 networkName=$1
 YQ="${2:-yq}"
 
-## Parse kind network subnet
-## Take only IPv4 subnets, exclude IPv6
-SUBNET=$(docker network inspect $networkName --format '{{json .IPAM.Config }}' | \
-    ${YQ} '.[] | select( .Subnet | test("^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}/\d+$")) | .Subnet')
+SUBNET=""
+if command -v podman &> /dev/null; then
+  SUBNET=$(podman network inspect kind | grep -Eo '"subnet": "[0-9.]+/[0-9]+' | awk -F\" '{print $4}' | head -n 1)
+elif command -v docker &> /dev/null; then
+  ## Parse kind network subnet
+  ## Take only IPv4 subnets, exclude IPv6
+  SUBNET=$(docker network inspect $networkName --format '{{json .IPAM.Config }}' | \
+      ${YQ} '.[] | select( .Subnet | test("^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}/\d+$")) | .Subnet')
+fi
+
 if [[ -z "$SUBNET" ]]; then
    echo "Error: parsing IPv4 network address for '$networkName' docker network"
    exit 1
@@ -22,7 +28,7 @@ fi
 
 # shellcheck disable=SC2206
 subnetParts=(${SUBNET//./ })
-cidr="${subnetParts[0]}.${subnetParts[1]}.200.0/24"
+cidr="${subnetParts[0]}.${subnetParts[1]}.0.252/30"
 
 cat <<EOF | ADDRESS=$cidr ${YQ} '(select(.kind == "IPAddressPool") | .spec.addresses[0]) = env(ADDRESS)'
 ---

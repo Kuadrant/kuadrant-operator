67bcfadac4331d271253dc294446f6b3fbc4fb8c
Jason Madigan <4467+jasonmadigan@users.noreply.github.com>
2024-03-28 15:17:55 +0000

    Podman fixes for v0.6.2 (#511)
    * Podman fixes
    
    Use subnet inside /24 network
    
    remove dead line
    
    * added podman support for docker-network-ipaddresspool.sh

--- utils/docker-network-ipaddresspool.sh
+++ utils/docker-network-ipaddresspool.sh
@@ -10,10 +10,21 @@ set -euo pipefail
 
 networkName=$1
 
-subnet=`docker network inspect $networkName -f '{{ (index .IPAM.Config 0).Subnet }}'`
+subnet=""
+if command -v podman &> /dev/null; then
+  subnet=`podman network inspect -f '{{range .Subnets}}{{if eq (len .Subnet.IP) 4}}{{.Subnet}}{{end}}{{end}}' kind`
+elif command -v docker &> /dev/null; then
+  subnet=`docker network inspect $networkName -f '{{ (index .IPAM.Config 0).Subnet }}'`
+fi
+
+if [[ -z "$subnet" ]]; then
+   echo "Error: parsing IPv4 network address for '$networkName' docker network"
+   exit 1
+fi
+
 # shellcheck disable=SC2206
 subnetParts=(${subnet//./ })
-cidr="${subnetParts[0]}.${subnetParts[1]}.200.0/24"
+cidr="${subnetParts[0]}.${subnetParts[1]}.0.252/30"
 
 cat <<EOF
 apiVersion: metallb.io/v1beta1

2b32ff929ea275fec37010eff83177b991955991
Jim Fitzpatrick <jfitzpat@redhat.com>
2024-05-09 11:40:55 +0100

    Refactor statement to PR comments.

--- controllers/authpolicy_authconfig.go
+++ controllers/authpolicy_authconfig.go
@@ -206,14 +206,11 @@ func routeGatewayAuthOverrides(t *kuadrantgatewayapi.Topology, ap *api.AuthPolic
 	// 4. is not marked for deletion
 	affectedPolicies = utils.Filter(affectedPolicies, func(policy kuadrantgatewayapi.Policy) bool {
 		p, ok := policy.(*api.AuthPolicy)
-		if !ok {
-			return false
-		}
-		if p.DeletionTimestamp != nil {
-			return false
-		}
-		return kuadrantgatewayapi.IsTargetRefGateway(policy.GetTargetRef()) &&
-			ap.GetUID() != policy.GetUID() && p.IsAtomicOverride()
+		return ok &&
+			p.DeletionTimestamp == nil &&
+			kuadrantgatewayapi.IsTargetRefGateway(policy.GetTargetRef()) &&
+			ap.GetUID() != policy.GetUID() &&
+			p.IsAtomicOverride()
 	})
 
 	return utils.Map(affectedPolicies, func(policy kuadrantgatewayapi.Policy) client.ObjectKey {

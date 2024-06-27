8d15814a3085def9bf8e4427af3766a8a8087573
KevFan <chfan@redhat.com>
2024-06-27 14:21:57 +0100

    feat: watch for issuer/clusterIssuer status changes for TLSPolicy

---
+++ controllers/tlspolicy_mappers.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/kuadrant/kuadrant-operator/api/v1alpha1"
+	"github.com/kuadrant/kuadrant-operator/pkg/library/utils"
+)
+
+func mapIssuerToPolicy(ctx context.Context, k8sClient client.Client, logger logr.Logger, object client.Object) []reconcile.Request {
+	_, ok := object.(*certmanagerv1.Issuer)
+	if !ok {
+		logger.V(1).Info("cannot map Issuer related event to tls policy", "error", fmt.Sprintf("%T is not a *certmanagerv1.Issuer", object))
+		return nil
+	}
+
+	policies := &v1alpha1.TLSPolicyList{}
+	if err := k8sClient.List(ctx, policies, client.InNamespace(object.GetNamespace())); err != nil {
+		logger.V(1).Error(err, "cannot list policies", "namespace", object.GetNamespace())
+		return nil
+	}
+
+	return policiesToRequests(logger, policies, object, certmanagerv1.IssuerKind)
+}
+
+func mapClusterIssuerToPolicy(ctx context.Context, k8sClient client.Client, logger logr.Logger, object client.Object) []reconcile.Request {
+	_, ok := object.(*certmanagerv1.ClusterIssuer)
+	if !ok {
+		logger.V(1).Info("cannot map ClusterIssuer related event to tls policy", "error", fmt.Sprintf("%T is not a *certmanagerv1.ClusterIssuer", object))
+		return nil
+	}
+
+	policies := &v1alpha1.TLSPolicyList{}
+	if err := k8sClient.List(ctx, policies); err != nil {
+		logger.V(1).Error(err, "cannot list policies", "namespace", object.GetNamespace())
+		return nil
+	}
+
+	return policiesToRequests(logger, policies, object, certmanagerv1.ClusterIssuerKind)
+}
+
+func policiesToRequests(logger logr.Logger, policies *v1alpha1.TLSPolicyList, object client.Object, issuerKind string) []reconcile.Request {
+	filteredPolicies := utils.Filter(policies.Items, func(policy v1alpha1.TLSPolicy) bool {
+		return policy.Spec.IssuerRef.Name == object.GetName() &&
+			policy.Spec.IssuerRef.Kind == issuerKind &&
+			policy.Spec.IssuerRef.Group == certmanagerv1.SchemeGroupVersion.Group
+	})
+
+	return utils.Map(filteredPolicies, func(p v1alpha1.TLSPolicy) reconcile.Request {
+		logger.V(1).Info("tls policy possibly affected by related event", "eventKind", issuerKind, "policyName", p.Name, "policyNamespace", p.Namespace)
+		return reconcile.Request{NamespacedName: client.ObjectKeyFromObject(&p)}
+	})
+}
---
+++ controllers/tlspolicy_mappers_test.go
@@ -0,0 +1,195 @@
+package controllers
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	v1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"github.com/go-logr/logr"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/kuadrant/kuadrant-operator/api/v1alpha1"
+)
+
+func Test_mapClusterIssuerToPolicy(t *testing.T) {
+	clusterIssuer := &certmanagerv1.ClusterIssuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-issuer",
+		},
+	}
+
+	s := runtime.NewScheme()
+	sb := runtime.NewSchemeBuilder(certmanagerv1.AddToScheme, v1alpha1.AddToScheme)
+	if err := sb.AddToScheme(s); err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		k8sClient client.Client
+		object    client.Object
+	}
+	tests := []struct {
+		name string
+		args args
+		want []reconcile.Request
+	}{
+		{
+			name: "not a cluster issuer",
+			args: args{
+				object: &certmanagerv1.Certificate{},
+			},
+			want: nil,
+		},
+		{
+			name: "map cluster issuer to matching policies",
+			args: args{
+				k8sClient: fake.NewClientBuilder().WithScheme(s).WithObjects(clusterIssuer).WithLists(testInitPolicies(clusterIssuer.Name, certmanagerv1.IssuerKind)).Build(),
+				object:    clusterIssuer,
+			},
+			want: []reconcile.Request{
+				{
+					NamespacedName: types.NamespacedName{Name: "p1", Namespace: "n1"},
+				},
+				{
+					NamespacedName: types.NamespacedName{Name: "p4", Namespace: "n2"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mapClusterIssuerToPolicy(context.Background(), tt.args.k8sClient, logr.Logger{}, tt.args.object); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mapClusterIssuerToPolicy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_mapIssuerToPolicy(t *testing.T) {
+	issuer := &certmanagerv1.Issuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-issuer",
+			Namespace: "n1",
+		},
+	}
+
+	s := runtime.NewScheme()
+	sb := runtime.NewSchemeBuilder(certmanagerv1.AddToScheme, v1alpha1.AddToScheme)
+	if err := sb.AddToScheme(s); err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		ctx       context.Context
+		k8sClient client.Client
+		logger    logr.Logger
+		object    client.Object
+	}
+	tests := []struct {
+		name string
+		args args
+		want []reconcile.Request
+	}{
+		{
+			name: "not an issuer",
+			args: args{
+				object: &certmanagerv1.Certificate{},
+			},
+			want: nil,
+		},
+		{
+			name: "map issuer to matching policies",
+			args: args{
+				k8sClient: fake.NewClientBuilder().WithScheme(s).WithObjects(issuer).WithLists(testInitPolicies(issuer.Name, certmanagerv1.IssuerKind)).Build(),
+				object:    issuer,
+			},
+			want: []reconcile.Request{
+				{
+					NamespacedName: types.NamespacedName{Name: "p1", Namespace: "n1"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mapIssuerToPolicy(tt.args.ctx, tt.args.k8sClient, tt.args.logger, tt.args.object); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mapIssuerToPolicy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func testInitPolicies(issuerName, issuerKind string) *v1alpha1.TLSPolicyList {
+	return &v1alpha1.TLSPolicyList{
+		Items: []v1alpha1.TLSPolicy{
+
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "p1",
+					Namespace: "n1",
+				},
+				Spec: v1alpha1.TLSPolicySpec{
+					CertificateSpec: v1alpha1.CertificateSpec{
+						IssuerRef: v1.ObjectReference{
+							Name:  issuerName,
+							Group: certmanagerv1.SchemeGroupVersion.Group,
+							Kind:  issuerKind,
+						},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "p2",
+					Namespace: "n1",
+				},
+				Spec: v1alpha1.TLSPolicySpec{
+					CertificateSpec: v1alpha1.CertificateSpec{
+						IssuerRef: v1.ObjectReference{
+							Name:  issuerName,
+							Group: "unknown.example.com",
+							Kind:  issuerKind,
+						},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "p3",
+					Namespace: "n1",
+				},
+				Spec: v1alpha1.TLSPolicySpec{
+					CertificateSpec: v1alpha1.CertificateSpec{
+						IssuerRef: v1.ObjectReference{
+							Name:  issuerName,
+							Group: certmanagerv1.SchemeGroupVersion.Group,
+							Kind:  "Unknown",
+						},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "p4",
+					Namespace: "n2",
+				},
+				Spec: v1alpha1.TLSPolicySpec{
+					CertificateSpec: v1alpha1.CertificateSpec{
+						IssuerRef: v1.ObjectReference{
+							Name:  issuerName,
+							Group: certmanagerv1.SchemeGroupVersion.Group,
+							Kind:  issuerKind,
+						},
+					},
+				},
+			},
+		},
+	}
+}

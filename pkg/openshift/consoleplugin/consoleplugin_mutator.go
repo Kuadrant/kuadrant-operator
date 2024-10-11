96912966824b61c4c9a0587342a064bfb944279e
Eguzki Astiz Lezaun <eastizle@redhat.com>
2024-10-11 10:37:36 +0200

    generic mutator
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- pkg/openshift/consoleplugin/consoleplugin_mutator.go
+++ pkg/openshift/consoleplugin/consoleplugin_mutator.go
@@ -1,13 +1,9 @@
 package consoleplugin
 
 import (
-	"fmt"
 	"reflect"
 
 	consolev1 "github.com/openshift/api/console/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	"github.com/kuadrant/kuadrant-operator/pkg/library/reconcilers"
 )
 
 func ServiceMutator(desired, existing *consolev1.ConsolePlugin) bool {
@@ -23,30 +19,4 @@ func ServiceMutator(desired, existing *consolev1.ConsolePlugin) bool {
 	}
 
 	return update
-}
-
-// ConsolePluginMutateFn is a function which mutates the existing ConsolePlugin into it's desired state.
-type MutateFn func(desired, existing *consolev1.ConsolePlugin) bool
-
-func Mutator(opts ...MutateFn) reconcilers.MutateFn {
-	return func(existingObj, desiredObj client.Object) (bool, error) {
-		existing, ok := existingObj.(*consolev1.ConsolePlugin)
-		if !ok {
-			return false, fmt.Errorf("%T is not a *consolev1.ConsolePlugin", existingObj)
-		}
-		desired, ok := desiredObj.(*consolev1.ConsolePlugin)
-		if !ok {
-			return false, fmt.Errorf("%T is not a *consolev1.ConsolePlugin", desiredObj)
-		}
-
-		update := false
-
-		// Loop through each option
-		for _, opt := range opts {
-			tmpUpdate := opt(desired, existing)
-			update = update || tmpUpdate
-		}
-
-		return update, nil
-	}
 }

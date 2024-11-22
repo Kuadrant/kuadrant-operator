f6f79b6fa1a859513d61d6f0635af157abbd4afe
maleck13 <maleck13@users.noreply.github.com>
2024-11-22 15:15:27 +0000

    Prepared release v1.0.0-rc7
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,17 @@
+#Release default values
+AUTHORINO_OPERATOR_VERSION=0.15.1
+AUTHORINO_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/authorino-operator-bundle:v0.15.1
+LIMITADOR_OPERATOR_VERSION=0.12.1
+LIMITADOR_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.12.1
+DNS_OPERATOR_VERSION=0.10.0
+DNS_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.10.0
+WASM_SHIM_VERSION=0.8.0
+RELATED_IMAGE_WASMSHIM=oci://quay.io/kuadrant/wasm-shim:v0.8.0
+RELATED_IMAGE_CONSOLEPLUGIN=quay.io/kuadrant/console-plugin:0.0.14
+IMG=quay.io/kuadrant/kuadrant-operator:v1.0.0-rc7
+BUNDLE_VERSION=1.0.0-rc7
+BUNDLE_IMG=quay.io/kuadrant/kuadrant-operator-bundle:v1.0.0-rc7
+CATALOG_IMG=quay.io/kuadrant/kuadrant-operator-catalog:v1.0.0-rc7
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=1.0.0-rc7

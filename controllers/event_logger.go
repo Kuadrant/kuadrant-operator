3457be8481cceb998efea5427113b0f2a27eed46
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-01-07 10:57:10 +0000

    REFACTOR: Event log message
    Refactor the event log message to not be as noisy when in Info log
    level. When there are a lot of resources the logs where not helpfully.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- controllers/event_logger.go
+++ controllers/event_logger.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"slices"
 	"sync"
 
 	"github.com/kuadrant/policy-machinery/controller"
@@ -16,13 +17,15 @@ func NewEventLogger() *EventLogger {
 
 func (e *EventLogger) Log(ctx context.Context, resourceEvents []controller.ResourceEvent, _ *machinery.Topology, err error, _ *sync.Map) error {
 	logger := controller.LoggerFromContext(ctx).WithName("event logger")
+	eventType := make(map[string]int, 0)
+	resources := make([]string, 0)
 	for _, event := range resourceEvents {
 		// log the event
 		obj := event.OldObject
 		if obj == nil {
 			obj = event.NewObject
 		}
-		logger.Info("new event",
+		logger.V(1).Info("new event",
 			"type", event.EventType.String(),
 			"kind", obj.GetObjectKind().GroupVersionKind().Kind,
 			"namespace", obj.GetNamespace(),
@@ -31,7 +34,20 @@ func (e *EventLogger) Log(ctx context.Context, resourceEvents []controller.Resou
 		if err != nil {
 			logger.Error(err, "error passed to reconcile")
 		}
+
+		_, ok := eventType[event.EventType.String()]
+		if ok {
+			eventType[event.EventType.String()]++
+		} else {
+			eventType[event.EventType.String()] = 1
+		}
+
+		if !slices.Contains(resources, obj.GetObjectKind().GroupVersionKind().Kind) {
+			resources = append(resources, obj.GetObjectKind().GroupVersionKind().Kind)
+		}
 	}
+
+	logger.Info("new events", "resources", resources, "eventTypes", eventType)
 
 	return nil
 }

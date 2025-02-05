b0a573a490b1bac32638509dfe9a4f45f32743ba
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-02-05 15:06:02 +0000

    UPDATE: Format release.toml
    Slight change to the form that is being used.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- utils/release/operator/update_catalog_source.sh
+++ utils/release/operator/update_catalog_source.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-v=quay.io/kuadrant/kuadrant-operator-catalog:v$(yq '.kuadrant.release' $env/release.toml) \
+v=quay.io/kuadrant/kuadrant-operator-catalog:v$(yq '.kuadrant-operator.version' $env/release.yaml) \
 	yq eval --inplace '.spec.image = strenv(v)' $env/config/deploy/olm/catalogsource.yaml	
---
+++ utils/release/pre-validation/verify_release_file.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Verifing the release file"
+file=$env/release.yaml
+if [[ -f $file ]]; then
+	echo "Release file exists"
+else
+	echo "Release file could not be found in $env"
+	exit 1
+fi
+
+has_error=0
+
+check_field() {
+    v=$(yq --unwrapScalar "$1" "$file" 2>/dev/null)
+    if [[ -z "$v" || "$v" == "null" ]]; then
+        echo "$2 is a required field. Please update."
+        has_error=1
+    fi
+}
+
+check_field ".olm.channels" "olm.channels"
+check_field ".olm.default-channel" "olm.default-channel"
+check_field ".kuadrant-operator.version" "kuadrant.version"
+check_field ".dependencies.authorino-operator" "dependencies.authorino-operator"
+check_field ".dependencies.console-plugin" "dependencies.console-plugin"
+check_field ".dependencies.dns-operator" "dependencies.dns-operator"
+check_field ".dependencies.limitador-operator" "dependencies.limitador-operator"
+check_field ".dependencies.wasm-shim" "dependencies.wasm-shim"
+
+exit $has_error

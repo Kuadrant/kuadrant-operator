2f015d9a4795041472d966210ee0b2f86055d8ce
David Martin <davmarti@redhat.com>
2025-02-06 11:07:18 +0000

    Checkpoint creation of istio & envoy gateway monitors
    Signed-off-by: David Martin <davmarti@redhat.com>

--- controllers/observability_reconciler.go
+++ controllers/observability_reconciler.go
@@ -15,13 +15,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-const kuadrantObservabilityLabel = "kuadrant-observability"
-const kOpMonitorName = "kuadrant-operator-monitor"
-const dnsOpMonitorName = "dns-operator-monitor"
-const authOpMonitorName = "authorino-operator-monitor"
-const limitOpMonitorName = "limitador-operator-monitor"
+const (
+	kuadrantObservabilityLabel = "kuadrant-observability"
+	kOpMonitorName             = "kuadrant-operator-monitor"
+	dnsOpMonitorName           = "dns-operator-monitor"
+	authOpMonitorName          = "authorino-operator-monitor"
+	limitOpMonitorName         = "limitador-operator-monitor"
+	istiodMonitorName          = "istiod-monitor"
+	istiodMonitorNS            = "istio-system"
+	istioPodMonitorName        = "istio-pod-monitor"
+	envoyGatewayMonitorName    = "envoy-gateway-monitor"
+	envoyGatewayMonitorNS      = "envoy-gateway-system"
+	envoyStatsMonitorName      = "envoy-stats-monitor"
+)
 
 var kOpMonitorSpec = &monitoringv1.ServiceMonitor{
 	TypeMeta: metav1.TypeMeta{
@@ -132,6 +141,152 @@ var limitOpMonitorSpec = &monitoringv1.ServiceMonitor{
 		},
 	},
 }
+var istiodMonitorSpec = &monitoringv1.ServiceMonitor{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       monitoringv1.ServiceMonitorsKind,
+		APIVersion: monitoringv1.SchemeGroupVersion.String(),
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name: istiodMonitorName,
+		Labels: map[string]string{
+			kuadrantObservabilityLabel: "true",
+		},
+	},
+	Spec: monitoringv1.ServiceMonitorSpec{
+		Endpoints: []monitoringv1.Endpoint{{
+			Port: "http-monitoring",
+		}},
+		Selector: metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "istiod",
+			},
+		},
+	},
+}
+
+var istioPodMonitorPortReplacement1 = `[$2]:$1`
+var istioPodMonitorPortReplacement2 = `$2:$1`
+var istioPodMonitorSpec = &monitoringv1.PodMonitor{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       monitoringv1.PodMonitorsKind,
+		APIVersion: monitoringv1.SchemeGroupVersion.String(),
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name: istioPodMonitorName,
+		Labels: map[string]string{
+			kuadrantObservabilityLabel: "true",
+		},
+	},
+	Spec: monitoringv1.PodMonitorSpec{
+		Selector: metav1.LabelSelector{
+			MatchExpressions: []metav1.LabelSelectorRequirement{
+				{
+					Key:      "istio-prometheus-ignore",
+					Operator: metav1.LabelSelectorOpDoesNotExist,
+				},
+			},
+		},
+		PodMetricsEndpoints: []monitoringv1.PodMetricsEndpoint{
+			{
+				Path:     "/stats/prometheus",
+				Interval: "30s",
+				RelabelConfigs: []monitoringv1.RelabelConfig{
+					{
+						Action:       "keep",
+						SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_pod_container_name"},
+						Regex:        "istio-proxy",
+					},
+					{
+						Action:       "keep",
+						SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_pod_annotationpresent_prometheus_io_scrape"},
+					},
+					{
+						Action: "replace",
+						SourceLabels: []monitoringv1.LabelName{
+							"__meta_kubernetes_pod_annotation_prometheus_io_port",
+							"__meta_kubernetes_pod_ip",
+						},
+						Regex:       `(\\d+);(([A-Fa-f0-9]{1,4}::?){1,7}[A-Fa-f0-9]{1,4})`,
+						Replacement: &istioPodMonitorPortReplacement1,
+						TargetLabel: "__address__",
+					},
+					{
+						Action: "replace",
+						SourceLabels: []monitoringv1.LabelName{
+							"__meta_kubernetes_pod_annotation_prometheus_io_port",
+							"__meta_kubernetes_pod_ip",
+						},
+						Regex:       `(\\d+);((([0-9]+?)(\\.|$)){4})`,
+						Replacement: &istioPodMonitorPortReplacement2,
+						TargetLabel: "__address__",
+					},
+					{
+						Action: "labeldrop",
+						Regex:  "__meta_kubernetes_pod_label_(.+)",
+					},
+					{
+						Action:       "replace",
+						SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_namespace"},
+						TargetLabel:  "namespace",
+					},
+					{
+						Action:       "replace",
+						SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_pod_name"},
+						TargetLabel:  "pod_name",
+					},
+				},
+			},
+		},
+	},
+}
+
+var envoyGatewayMonitorSpec = &monitoringv1.ServiceMonitor{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       monitoringv1.ServiceMonitorsKind,
+		APIVersion: monitoringv1.SchemeGroupVersion.String(),
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name: envoyGatewayMonitorName,
+		Labels: map[string]string{
+			kuadrantObservabilityLabel: "true",
+		},
+	},
+	Spec: monitoringv1.ServiceMonitorSpec{
+		Endpoints: []monitoringv1.Endpoint{{
+			Port: "metrics",
+		}},
+		Selector: metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"control-plane": "envoy-gateway",
+			},
+		},
+	},
+}
+
+var envoyStatsPort = "http-envoy-prom"
+var envoyStatsMonitorSpec = &monitoringv1.PodMonitor{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       monitoringv1.PodMonitorsKind,
+		APIVersion: monitoringv1.SchemeGroupVersion.String(),
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name: envoyStatsMonitorName,
+		Labels: map[string]string{
+			kuadrantObservabilityLabel: "true",
+		},
+	},
+	Spec: monitoringv1.PodMonitorSpec{
+		PodMetricsEndpoints: []monitoringv1.PodMetricsEndpoint{{
+			Port: &envoyStatsPort,
+			Path: "/stats/prometheus",
+		}},
+		Selector: metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "kuadrant-ingressgateway",
+			},
+		},
+	},
+}
 
 type ObservabilityReconciler struct {
 	Client     *dynamic.DynamicClient
@@ -178,74 +333,107 @@ func (r *ObservabilityReconciler) Reconcile(ctx context.Context, _ []controller.
 	logger.V(1).Info("observability enabled, creating monitors")
 
 	// Kuadrant Operator monitor
-	err := r.createServiceMonitor(ctx, monitorObjs, kOpMonitorName, kOpMonitorSpec, kObj, logger)
-	if err != nil {
+	if err := r.createMonitor(ctx, monitorObjs, kOpMonitorSpec, kObj.Namespace, logger); err != nil {
 		logger.Error(err, "failed to create kuadrant operator monitor", "status", "error")
 		return err
 	}
-	logger.V(1).Info("kuadrant operator monitor created")
 
 	// DNS Operator monitor
-	err = r.createServiceMonitor(ctx, monitorObjs, dnsOpMonitorName, dnsOpMonitorSpec, kObj, logger)
-	if err != nil {
+	if err := r.createMonitor(ctx, monitorObjs, dnsOpMonitorSpec, kObj.Namespace, logger); err != nil {
 		logger.Error(err, "failed to create dns operator monitor", "status", "error")
 		return err
 	}
-	logger.V(1).Info("dns operator monitor created")
 
 	// Authorino operator monitor
-	err = r.createServiceMonitor(ctx, monitorObjs, authOpMonitorName, authOpMonitorSpec, kObj, logger)
-	if err != nil {
+	if err := r.createMonitor(ctx, monitorObjs, authOpMonitorSpec, kObj.Namespace, logger); err != nil {
 		logger.Error(err, "failed to create authorino operator monitor", "status", "error")
 		return err
 	}
-	logger.V(1).Info("authorino operator monitor created")
 
 	// Limitador operator monitor
-	err = r.createServiceMonitor(ctx, monitorObjs, limitOpMonitorName, limitOpMonitorSpec, kObj, logger)
-	if err != nil {
+	if err := r.createMonitor(ctx, monitorObjs, limitOpMonitorSpec, kObj.Namespace, logger); err != nil {
 		logger.Error(err, "failed to create limitador operator monitor", "status", "error")
 		return err
 	}
-	logger.V(1).Info("limitador operator monitor created")
 
-	// TODO: Create monitors for gateway provider
+	// Create monitors for each gateway instance
+	gateways := lo.Map(topology.Targetables().Items(func(o machinery.Object) bool {
+		return o.GroupVersionKind().GroupKind() == machinery.GatewayGroupKind
+	}), func(g machinery.Targetable, _ int) *machinery.Gateway {
+		return g.(*machinery.Gateway)
+	})
+
+	for _, gateway := range gateways {
+		gatewayClasses := topology.All().Parents(gateway)
+		logger.V(1).Info(fmt.Sprintf("found %d gateway classes for gateway %s/%s", len(gatewayClasses), gateway.Namespace, gateway.Name))
+		if len(gatewayClasses) != 1 {
+			// must be 1 parent only
+			continue
+		}
+		gatewayClass := gatewayClasses[0].(*machinery.GatewayClass)
+		if gatewayClass.GatewayClass.Spec.ControllerName == istioGatewayControllerName {
+			if err := r.createMonitor(ctx, monitorObjs, istiodMonitorSpec, istiodMonitorNS, logger); err != nil {
+				logger.Error(err, "failed to create istiod monitor", "status", "error")
+				return err
+			}
+			if err := r.createMonitor(ctx, monitorObjs, istioPodMonitorSpec, gateway.Namespace, logger); err != nil {
+				logger.Error(err, "failed to create istio pod monitor", "status", "error")
+				return err
+			}
+		} else if gatewayClass.GatewayClass.Spec.ControllerName == envoyGatewayGatewayControllerName {
+			if err := r.createMonitor(ctx, monitorObjs, envoyGatewayMonitorSpec, envoyGatewayMonitorNS, logger); err != nil {
+				logger.Error(err, "failed to create envoy gateway monitor", "status", "error")
+				return err
+			}
+			if err := r.createMonitor(ctx, monitorObjs, envoyStatsMonitorSpec, gateway.Namespace, logger); err != nil {
+				logger.Error(err, "failed to create envoy stats monitor", "status", "error")
+				return err
+			}
+		}
+	}
 
 	return nil
 }
 
-func (r *ObservabilityReconciler) createServiceMonitor(ctx context.Context, monitorObjs []machinery.Object, smName string, sm *monitoringv1.ServiceMonitor, kObj *kuadrantv1beta1.Kuadrant, logger logr.Logger) error {
-	_, monitor := lo.Find(monitorObjs, func(item machinery.Object) bool {
-		return item.GroupVersionKind().Kind == monitoringv1.ServiceMonitorsKind && item.GetName() == smName
+func (r *ObservabilityReconciler) createMonitor(ctx context.Context, monitorObjs []machinery.Object, monitor client.Object, ns string, logger logr.Logger) error {
+	_, monitorExists := lo.Find(monitorObjs, func(item machinery.Object) bool {
+		return item.GroupVersionKind().Kind == monitor.GetObjectKind().GroupVersionKind().Kind && item.GetName() == monitor.GetName() && item.GetNamespace() == ns
 	})
-	if monitor {
-		logger.V(1).Info(fmt.Sprintf("monitor already exists %s, skipping create", smName))
+	if monitorExists {
+		logger.V(1).Info(fmt.Sprintf("monitor already exists %s/%s, skipping create", ns, monitor.GetName()))
 		return nil
 	} else {
-		logger.V(1).Info(fmt.Sprintf("creating monitor %s", smName))
-		obj, err := controller.Destruct(sm)
+		logger.V(1).Info(fmt.Sprintf("creating monitor %s", monitor.GetName()))
+		obj, err := controller.Destruct(monitor)
 		if err != nil {
 			return err
 		}
 
-		_, err = r.Client.Resource(monitoringv1.SchemeGroupVersion.WithResource("servicemonitors")).Namespace(kObj.Namespace).Create(ctx, obj, metav1.CreateOptions{})
+		mapping, err := r.restMapper.RESTMapping(monitor.GetObjectKind().GroupVersionKind().GroupKind())
+		if err != nil {
+			logger.Error(err, "failed to get monitor restmapping", "status", "error")
+			return err
+		}
+		if _, err = r.Client.Resource(mapping.Resource).Namespace(ns).Create(ctx, obj, metav1.CreateOptions{}); err == nil {
+			logger.V(1).Info(fmt.Sprintf("created monitor %s", monitor.GetName()))
+		}
 		return err
 	}
 }
 
 func (r *ObservabilityReconciler) deleteAllMonitors(ctx context.Context, monitorObjs []machinery.Object, logger logr.Logger) {
-	lo.ForEach(monitorObjs, func(monitor machinery.Object, index int) {
+	for _, monitor := range monitorObjs {
 		logger.V(1).Info(fmt.Sprintf("deleting monitor %s %s/%s", monitor.GroupVersionKind().Kind, monitor.GetNamespace(), monitor.GetName()))
 		mapping, err := r.restMapper.RESTMapping(monitor.GroupVersionKind().GroupKind())
 		if err != nil {
 			logger.Error(err, "failed to get monitor restmapping", "status", "error")
 			return
 		}
-		err = r.Client.Resource(mapping.Resource).Namespace(monitor.GetNamespace()).Delete(ctx, monitor.GetName(), metav1.DeleteOptions{})
-		if err != nil {
+		if err = r.Client.Resource(mapping.Resource).Namespace(monitor.GetNamespace()).Delete(ctx, monitor.GetName(), metav1.DeleteOptions{}); err != nil {
 			logger.Error(err, "failed to delete monitor", "status", "error")
+			return
 		}
 		logger.V(1).Info(fmt.Sprintf("deleted monitor %s %s/%s", monitor.GroupVersionKind().Kind, monitor.GetNamespace(), monitor.GetName()))
-	})
+	}
 
 }

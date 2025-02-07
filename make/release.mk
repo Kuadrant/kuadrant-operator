b6a0d8c8a67ed76f9816e1eae67bd1492d97446a
didierofrivia <4183971+didierofrivia@users.noreply.github.com>
2025-02-07 15:15:07 +0000

    Prepared release v1.1.0-alpha1
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ make/release.mk
@@ -0,0 +1,17 @@
+#Release default values
+AUTHORINO_OPERATOR_VERSION=0.17.0-alpha1
+AUTHORINO_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/authorino-operator-bundle:v0.17.0-alpha1
+LIMITADOR_OPERATOR_VERSION=0.13.0-alpha1
+LIMITADOR_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:v0.13.0-alpha1
+DNS_OPERATOR_VERSION=0.13.0-alpha1
+DNS_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.13.0-alpha1
+WASM_SHIM_VERSION=0.8.1
+RELATED_IMAGE_WASMSHIM=oci://quay.io/kuadrant/wasm-shim:v0.8.1
+RELATED_IMAGE_CONSOLEPLUGIN=quay.io/kuadrant/console-plugin:v0.0.18
+IMG=quay.io/kuadrant/kuadrant-operator:v1.1.0-alpha1
+BUNDLE_VERSION=1.1.0-alpha1
+BUNDLE_IMG=quay.io/kuadrant/kuadrant-operator-bundle:v1.1.0-alpha1
+CATALOG_IMG=quay.io/kuadrant/kuadrant-operator-catalog:v1.1.0-alpha1
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=1.1.0-alpha1

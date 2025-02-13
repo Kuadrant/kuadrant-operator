ddb98a13be26dc0441c99355d628cdde7e8f048c
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-02-13 16:21:13 +0000

    testing
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

---
+++ make/release.mk
@@ -0,0 +1,17 @@
+#Release default values
+AUTHORINO_OPERATOR_VERSION=0.16.0
+AUTHORINO_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/authorino-operator-bundle:latest
+LIMITADOR_OPERATOR_VERSION=0.12.1
+LIMITADOR_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/limitador-operator-bundle:latest
+DNS_OPERATOR_VERSION=0.12.0
+DNS_OPERATOR_BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:latest
+WASM_SHIM_VERSION=0.8.1
+RELATED_IMAGE_WASMSHIM=oci://quay.io/kuadrant/wasm-shim:latest
+RELATED_IMAGE_CONSOLEPLUGIN=quay.io/kuadrant/console-plugin:v0.0.18
+IMG=quay.io/kuadrant/kuadrant-operator:latest
+BUNDLE_VERSION=0.0.0
+BUNDLE_IMG=quay.io/kuadrant/kuadrant-operator-bundle:latest
+CATALOG_IMG=quay.io/kuadrant/kuadrant-operator-catalog:latest
+CHANNELS=alpha
+BUNDLE_CHANNELS=--channels=alpha
+VERSION=1.0.2

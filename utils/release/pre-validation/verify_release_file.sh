34a3385f70cecd38efad55bb272fb18b43b94973
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-02-14 12:01:50 +0000

    Changes from PR comments
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- utils/release/pre-validation/verify_release_file.sh
+++ utils/release/pre-validation/verify_release_file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Verifing the release file"
-file=$env/release.toml
+file=$env/release.yaml
 if [[ -f $file ]]; then
 	echo "Release file exists"
 else
@@ -12,15 +12,15 @@ fi
 has_error=0
 
 check_field() {
-    v=$(yq --output-format=yaml --unwrapScalar "$1" "$file" 2>/dev/null)
+    v=$(yq --unwrapScalar "$1" "$file" 2>/dev/null)
     if [[ -z "$v" || "$v" == "null" ]]; then
         echo "$2 is a required field. Please update."
         has_error=1
     fi
 }
 
-check_field ".kuadrant.olm.channels" "kuadrant.olm.channels"
-check_field ".kuadrant.olm.default_channel" "kuadrant.olm.default_channel"
+check_field ".olm.channels" "olm.channels"
+check_field ".olm.default_channel" "olm.default_channel"
 check_field ".kuadrant.release" "kuadrant.release"
 check_field ".dependencies.Authorino" "dependencies.Authorino"
 check_field ".dependencies.Console_plugin" "dependencies.Console_plugin"

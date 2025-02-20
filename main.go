daa02774caf24941c6afa241baee47d631a2f8e9
Laura Fitzgerald <lfitzger@redhat.com>
2025-02-20 13:05:53 +0000

    adding mtls config
    Signed-off-by: Laura Fitzgerald <lfitzger@redhat.com>

--- main.go
+++ main.go
@@ -32,6 +32,8 @@ import (
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	istioextensionv1alpha1 "istio.io/client-go/pkg/apis/extensions/v1alpha1"
 	istionetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	istiosecurity "istio.io/client-go/pkg/apis/security/v1"
+
 	corev1 "k8s.io/api/core/v1"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
@@ -78,6 +80,7 @@ func init() {
 	utilruntime.Must(egv1alpha1.AddToScheme(scheme))
 	utilruntime.Must(consolev1.AddToScheme(scheme))
 	utilruntime.Must(monitoringv1.AddToScheme(scheme))
+	utilruntime.Must(istiosecurity.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 
 	logger := log.NewLogger(

c1d12d8c30a141c6c5e7d520b4c606fbb10d90a9
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-02-25 08:42:49 +0000

    REFACTOR: Naming
    Change the naming of components within the release.yaml file.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- utils/release/operator/update_catalog_source.sh
+++ utils/release/operator/update_catalog_source.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-v=quay.io/kuadrant/kuadrant-operator-catalog:v$(yq '.kuadrant.release' $env/release.yaml) \
+v=quay.io/kuadrant/kuadrant-operator-catalog:v$(yq '.kuadrant-operator.release' $env/release.yaml) \
 	yq eval --inplace '.spec.image = strenv(v)' $env/config/deploy/olm/catalogsource.yaml	

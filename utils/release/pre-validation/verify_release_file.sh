8d8a86fbf25a2e6d6ad8aacf0d62423c3055eb83
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-02-25 16:16:58 +0000

    FIX: rename issue
    Some instances were missed in the rename.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- utils/release/pre-validation/verify_release_file.sh
+++ utils/release/pre-validation/verify_release_file.sh
@@ -21,7 +21,7 @@ check_field() {
 
 check_field ".olm.channels" "olm.channels"
 check_field ".olm.default-channel" "olm.default-channel"
-check_field ".kuadrant.version" "kuadrant.version"
+check_field ".kuadrant-operator.version" "kuadrant.version"
 check_field ".dependencies.authorino-operator" "dependencies.authorino-operator"
 check_field ".dependencies.console-plugin" "dependencies.console-plugin"
 check_field ".dependencies.dns-operator" "dependencies.dns-operator"

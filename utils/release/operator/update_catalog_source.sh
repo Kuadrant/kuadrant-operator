87db23c74455bb6cbd6688d4ea353c982830c8a9
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-03-05 14:19:07 +0000

    UPDATE: Latest tag
    In the release.yaml 0.0.0 can now be used to mark a version should be
    the latest version.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- utils/release/operator/update_catalog_source.sh
+++ utils/release/operator/update_catalog_source.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+mod_version() {
+  version=$1
+  if [ "$version" == "0.0.0" ]; then
+    echo "latest"
+  else
+    echo "v$version"
+  fi
+}
 
-v=quay.io/kuadrant/kuadrant-operator-catalog:v$(yq '.kuadrant-operator.version' $env/release.yaml) \
+v=quay.io/kuadrant/kuadrant-operator-catalog:$(mod_version $(yq '.kuadrant-operator.version' $env/release.yaml)) \
 	yq eval --inplace '.spec.image = strenv(v)' $env/config/deploy/olm/catalogsource.yaml	

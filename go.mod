6622320a4de82ca09ab063e5d0e092c74e2f9f5b
Alex Snaps <alex@wcgw.dev>
2025-03-06 15:26:14 -0500

    extensions: Slowly emerging EmbeddedPlugins
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/elliotchance/orderedmap/v2 v2.2.0
 	github.com/envoyproxy/gateway v1.1.0
 	github.com/go-logr/logr v1.4.2
+	github.com/google/cel-go v0.21.0
 	github.com/google/go-cmp v0.6.0
 	github.com/kuadrant/authorino v0.20.0
 	github.com/kuadrant/authorino-operator v0.11.1
@@ -21,7 +22,8 @@ require (
 	github.com/prometheus/client_golang v1.20.4
 	github.com/samber/lo v1.47.0
 	go.uber.org/zap v1.27.0
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.4
 	gotest.tools v2.2.0+incompatible
 	istio.io/api v1.22.3-0.20240703105953-437a88321a16
 	istio.io/client-go v1.22.3-0.20240703110620-5f69a1e4c030
@@ -38,6 +40,7 @@ require (
 )
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -73,20 +76,22 @@ require (
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/telepresenceio/watchable v0.0.0-20220726211108-9bb86f92afa7 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/oauth2 v0.23.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.25.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ internal/extension/oop.go
@@ -0,0 +1,160 @@
+/*
+Copyright 2025 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package extension
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+
+	extpb "github.com/kuadrant/kuadrant-operator/pkg/extension/grpc/v0"
+
+	"github.com/go-logr/logr"
+	"google.golang.org/grpc"
+)
+
+const defaultUnixSocket = ".grpc.sock"
+
+type OOPExtension struct {
+	name       string
+	executable string
+	socket     string
+	cmd        *exec.Cmd
+	server     *grpc.Server
+	service    extpb.ExtensionServiceServer
+	logger     logr.Logger
+}
+
+func NewOOPExtension(name string, location string, service extpb.ExtensionServiceServer, logger logr.Logger) (OOPExtension, error) {
+	var err error
+	var stat os.FileInfo
+
+	executable := fmt.Sprintf("%s/%s/%s", location, name, name)
+	if stat, err = os.Stat(executable); err == nil {
+		if stat.IsDir() || stat.Mode()&0111 == 0 {
+			err = fmt.Errorf("%s: Not an executable", executable)
+		}
+	}
+
+	return OOPExtension{
+		name:       name,
+		socket:     fmt.Sprintf("%s/%s/%s", location, name, defaultUnixSocket),
+		executable: executable,
+		service:    service,
+		logger:     logger.WithName(name),
+	}, err
+}
+
+func (p *OOPExtension) Name() string {
+	return p.name
+}
+
+func (p *OOPExtension) Start() error {
+	p.logger.Info("starting...")
+
+	if err := p.startServer(); err != nil {
+		return err
+	}
+
+	cmd := exec.Command(p.executable, p.socket) // #nosec G204
+	if err := cmd.Start(); err != nil {
+		if e := p.stopServer(); e != nil {
+			p.logger.Error(e, "failed starting process, then stopping gRPC server failed")
+		}
+		return err
+	}
+	p.logger.Info("started")
+
+	// only set this, if we successfully started it all
+	p.cmd = cmd
+	return nil
+}
+
+func (p *OOPExtension) IsAlive() bool {
+	return p.cmd != nil && p.cmd.Process.Signal(syscall.Signal(0)) == nil
+}
+
+func (p *OOPExtension) Stop() error {
+	p.logger.Info("stopping...")
+	var err error
+
+	// Did we ever successfully started?
+	if p.cmd != nil {
+		if err = p.cmd.Process.Signal(syscall.SIGTERM); err == nil {
+			timer := time.AfterFunc(2*time.Second, func() {
+				_ = p.cmd.Process.Kill() // we know this can fail, as this is racy. All that really matters is the `Wait()` below
+			})
+
+			if e := p.cmd.Wait(); e != nil {
+				status := p.cmd.ProcessState.Sys().(syscall.WaitStatus)
+				if !status.Signaled() || status.Signal() != syscall.SIGTERM {
+					err = e
+				}
+			}
+
+			timer.Stop()
+		}
+
+		if e := p.stopServer(); e != nil {
+			if err == nil {
+				err = e
+			} else {
+				p.logger.Error(e, "stopping gRPC server failed, while shutting down the process also failed")
+			}
+		}
+		p.logger.Info("stopped")
+		p.cmd = nil
+	} else {
+		p.logger.Info("nothing to stop")
+	}
+
+	return err
+}
+
+func (p *OOPExtension) startServer() error {
+	if p.server == nil {
+		ln, err := net.Listen("unix", p.socket)
+		if err != nil {
+			return err
+		}
+
+		p.server = grpc.NewServer()
+		extpb.RegisterExtensionServiceServer(p.server, p.service)
+
+		go func() {
+			if err := p.server.Serve(ln); err != nil {
+				// FIXME: Make this fail synchronously somehow
+				p.logger.Error(err, "failed to start server")
+			}
+		}()
+	}
+	return nil
+}
+
+func (p *OOPExtension) stopServer() error {
+	if p.server != nil {
+		p.server.Stop()
+		p.server = nil
+		if _, err := os.Stat(p.socket); err == nil {
+			return os.Remove(p.socket)
+		}
+	}
+	return nil
+}

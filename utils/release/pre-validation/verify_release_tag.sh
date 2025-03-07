628d778c1b08bd4dc30b0b1860ab7da234cc013a
dd di cesare <didi@posteo.net>
2025-03-07 17:54:38 +0100

    [refactor] Using authenticated request and json verification
    * In order to avoid rate limiting and empty json
    
    Signed-off-by: dd di cesare <didi@posteo.net>

--- utils/release/pre-validation/verify_release_tag.sh
+++ utils/release/pre-validation/verify_release_tag.sh
@@ -4,9 +4,18 @@ echo "Verifying if the Operator tag has been already released"
 file=$env/release.yaml
 kuadrant_operator_tag=v$(yq "(.kuadrant-operator.version)" "$file")
 
-response=$(curl -s https://api.github.com/repos/kuadrant/kuadrant-operator/tags | jq 'any(.[]; .name == "'$kuadrant_operator_tag'")')
+response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/kuadrant/kuadrant-operator/tags")
 
-if [[ $response == "true"  ]]; then
-  echo "Kuadrant operator tag $kuadrant_operator_tag already exists, stopping release process"
+# Check if response contains valid JSON array
+if ! echo "$response" | jq empty > /dev/null 2>&1; then
+  echo "Failed to fetch tags for kuadrant-operator"
   exit 1
+fi
+
+# Check if specific tag exists
+if [[ $( echo "$response" | jq 'any(.[]; .name == "'$kuadrant_operator_tag'")' ) == true ]]; then
+  echo "kuadrant-operator tag $kuadrant_operator_tag already exists, stopping the release process"
+  exit 1
+else
+  echo "kuadrant-operator tag $kuadrant_operator_tag doesn't exists, continuing the release process"
 fi

44c15f3cdaa588ee8796863e8ab9da7906f7d9a8
dd di cesare <didi@posteo.net>
2025-03-10 09:57:23 +0100

    [script] Conditionally adding auth header
    Signed-off-by: dd di cesare <didi@posteo.net>

--- utils/release/pre-validation/verify_release_tag.sh
+++ utils/release/pre-validation/verify_release_tag.sh
@@ -4,7 +4,11 @@ echo "Verifying if the Operator tag has been already released"
 file=$env/release.yaml
 kuadrant_operator_tag=v$(yq "(.kuadrant-operator.version)" "$file")
 
-response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/kuadrant/kuadrant-operator/tags")
+if [[ $GITHUB_TOKEN ]]; then
+  response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/kuadrant/kuadrant-operator/tags")
+else
+  response=$(curl -s "https://api.github.com/repos/kuadrant/kuadrant-operator/tags")
+fi
 
 # Check if response contains valid JSON array
 if ! echo "$response" | jq empty > /dev/null 2>&1; then

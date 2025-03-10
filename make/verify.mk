e06d29c2da640ec69e30d87e1b866f9b6ee50f75
dd di cesare <didi@posteo.net>
2025-03-10 20:42:34 +0100

    [make] Restore original verification targets
    * With some modifications. verify-manfiests calls the targets verify
      bundle, controller-gen and helm
    
    Signed-off-by: dd di cesare <didi@posteo.net>

--- make/verify.mk
+++ make/verify.mk
@@ -16,7 +16,28 @@ verify-go-mod: ## Verify go.mod matches source code
 	go mod tidy
 	git diff --exit-code ./go.mod
 
-.PHONY: verify-prepare-release
+.PHONY: verify-controller-manifests
+verify-controller-manifests: manifests ## Verify controller-gen manifests update.
+	git diff --exit-code ./config
+	[ -z "$$(git ls-files --other --exclude-standard --directory --no-empty-directory ./config)" ]
+
+.PHONY: verify-bundle
+verify-bundle: bundle ## Verify bundle update.
+	git diff --exit-code ./bundle
+	[ -z "$$(git ls-files --other --exclude-standard --directory --no-empty-directory ./bundle)" ]
+
+.PHONY: verify-helm-charts
+verify-helm-charts: helm-build ## Verify helm charts update.
+	git diff --exit-code ./charts
+	[ -z "$$(git ls-files --other --exclude-standard --directory --no-empty-directory ./charts)" ]
+
+.PHONY: verify-manifests ## Verify controller-gen, bundle and helm charts manifests.
+verify-manifests: ## Verify manifests update.
+	make verify-controller-manifests
+	make verify-bundle
+	make verify-helm-charts
+
+.PHONY: verify-prepare-release ## Verify set of manifests based on release.yaml file.
 verify-prepare-release: prepare-release
 	git diff --exit-code .
 

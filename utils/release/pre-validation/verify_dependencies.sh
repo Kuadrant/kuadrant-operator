a79fdf2abb61a4e2d67cd27df206a669eceaca42
dd di cesare <didi@posteo.net>
2025-03-10 09:57:23 +0100

    [script] Conditionally adding auth header
    Signed-off-by: dd di cesare <didi@posteo.net>

--- utils/release/pre-validation/verify_dependencies.sh
+++ utils/release/pre-validation/verify_dependencies.sh
@@ -4,17 +4,24 @@ echo "Verifying if the dependencies have been already released"
 file=$env/release.yaml
 dependencies=("authorino-operator" "console-plugin" "dns-operator" "limitador-operator" "wasm-shim")
 
+auth_header=""
+if [[ $GITHUB_TOKEN ]]; then
+  auth_header="-H \"Authorization: Bearer $GITHUB_TOKEN\""
+fi
+
 for dependency in "${dependencies[@]}"; do
   dependency_tag=v$(yq "(.dependencies.$dependency)" "$file")
 
   echo "Checking dependency $dependency tag $dependency_tag"
 
-  # Get all tags and store raw response
+  url="https://api.github.com/repos/kuadrant/$dependency/tags"
+
   if [[ $dependency == "console-plugin" ]]; then
-    response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/kuadrant/kuadrant-$dependency/tags")
-  else
-    response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/kuadrant/$dependency/tags")
+    url="https://api.github.com/repos/kuadrant/kuadrant-$dependency/tags"
   fi
+
+  # Get all tags and store raw response
+  response=$(curl -s $auth_header "$url")
 
   # Check if response contains valid JSON array
   if ! echo "$response" | jq empty > /dev/null 2>&1; then

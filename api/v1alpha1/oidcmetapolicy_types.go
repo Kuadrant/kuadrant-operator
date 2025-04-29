e7f5f0d33b23bdb8d76806d5d0344d0ec30799ac
dd di cesare <didi@posteo.net>
2025-04-29 09:26:08 +0200

    [crd] Minimal API for OIDC
    Signed-off-by: dd di cesare <didi@posteo.net>

--- api/v1alpha1/oidcmetapolicy_types.go
+++ api/v1alpha1/oidcmetapolicy_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	gatewayapiv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -25,11 +26,24 @@ import (
 
 // OIDCMetaPolicySpec defines the desired state of OIDCMetaPolicy
 type OIDCMetaPolicySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Reference to the object to which this policy applies.
+	// +kubebuilder:validation:XValidation:rule="self.group == 'gateway.networking.k8s.io'",message="Invalid targetRef.group. The only supported value is 'gateway.networking.k8s.io'"
+	// +kubebuilder:validation:XValidation:rule="self.kind == 'HTTPRoute' || self.kind == 'Gateway'",message="Invalid targetRef.kind. The only supported values are 'HTTPRoute' and 'Gateway'"
+	TargetRef gatewayapiv1alpha2.LocalPolicyTargetReferenceWithSectionName `json:"targetRef"`
 
-	// Foo is an example field of OIDCMetaPolicy. Edit oidcmetapolicy_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Bare set of policy rules (implicit defaults).
+	OIDCMetaPolicySpecProper `json:""`
+}
+
+// OIDCMetaPolicySpecProper contains common shared fields for the future inclusion of defaults and overrides
+type OIDCMetaPolicySpecProper struct {
+	// Provider holds the information for the OIDC provider
+	Provider *Provider `json:"provider,omitempty"`
+}
+
+type Provider struct {
+	IssuerURL string `json:"issuerURL"`
+	ClientID  string `json:"clientID"`
 }
 
 // OIDCMetaPolicyStatus defines the observed state of OIDCMetaPolicy

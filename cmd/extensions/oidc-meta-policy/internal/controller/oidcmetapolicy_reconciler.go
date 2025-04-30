1ad0c478f1665a762008a68072c3cee1bab96ff4
dd di cesare <didi@posteo.net>
2025-04-30 09:54:33 +0200

    [artifacts] Reconciler structure, bundle and charts with new CRD
    Signed-off-by: dd di cesare <didi@posteo.net>

---
+++ cmd/extensions/oidc-meta-policy/internal/controller/oidcmetapolicy_reconciler.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/kuadrant/kuadrant-operator/pkg/extension/types"
+	"github.com/kuadrant/kuadrant-operator/pkg/extension/utils"
+
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type OIDCMetaPolicyReconciler struct {
+}
+
+func (e *OIDCMetaPolicyReconciler) Reconcile(ctx context.Context, _ reconcile.Request, _ *types.KuadrantCtx) (reconcile.Result, error) {
+	logger := utils.LoggerFromContext(ctx).WithName("OIDCMetaPolicyReconciler")
+	logger.Info("Reconciling OIDCMetaPolicy")
+
+	_, err := utils.DynamicClientFromContext(ctx)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	return reconcile.Result{}, nil
+}

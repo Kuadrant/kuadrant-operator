13ea4f2e9f5ab3d589d4edbb01bd4635beb23ac0
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-05-08 08:41:06 +0100

    ADD: Pod Disruption Budget
    Add  the ability to configure the pdb for limitador.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- internal/controller/resilient_deployment_workflow_test.go
+++ internal/controller/resilient_deployment_workflow_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	limitadorv1alpha1 "github.com/kuadrant/limitador-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	kuadrantv1beta1 "github.com/kuadrant/kuadrant-operator/api/v1beta1"
 )
@@ -47,4 +48,64 @@ func TestResilienceCounterStorageReconciler_isConfigured(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestLimitadorPDBIsConfigured(t *testing.T) {
+	testCases := []struct {
+		name     string
+		lObj     *limitadorv1alpha1.Limitador
+		expected bool
+	}{
+		{
+			name:     "limitador object is nil",
+			expected: false,
+			lObj:     nil,
+		},
+		{
+			name:     "limitador PBD spec is nil",
+			expected: false,
+			lObj:     &limitadorv1alpha1.Limitador{},
+		},
+		{
+			name:     "limitador PBD spec is not nil, but empty",
+			expected: false,
+			lObj: &limitadorv1alpha1.Limitador{
+				Spec: limitadorv1alpha1.LimitadorSpec{
+					PodDisruptionBudget: &limitadorv1alpha1.PodDisruptionBudgetType{},
+				},
+			},
+		},
+		{
+			name:     "limitador PDB MaxUnaviaible is set",
+			expected: true,
+			lObj: &limitadorv1alpha1.Limitador{
+				Spec: limitadorv1alpha1.LimitadorSpec{
+					PodDisruptionBudget: &limitadorv1alpha1.PodDisruptionBudgetType{
+						MaxUnavailable: &intstr.IntOrString{IntVal: 1},
+					},
+				},
+			},
+		},
+		{
+			name:     "limitador PDB MinAviaible is set",
+			expected: true,
+			lObj: &limitadorv1alpha1.Limitador{
+				Spec: limitadorv1alpha1.LimitadorSpec{
+					PodDisruptionBudget: &limitadorv1alpha1.PodDisruptionBudgetType{
+						MinAvailable: &intstr.IntOrString{IntVal: 1},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(subT *testing.T) {
+			result := limitadorPDBIsConfigured(tc.lObj)
+			if result != tc.expected {
+				subT.Fatalf("limitadorPDBIsConfigured result not as expected. Expected: %v, Actual: %v", tc.expected, result)
+			}
+		})
+	}
+
 }

aa9d26c8d641b682dd93cb9f3f3173a2a5efc5d7
Adam Cattermole <acatterm@redhat.com>
2025-06-13 15:13:51 +0100

    Update Policy interface and create AddToData
    Signed-off-by: Adam Cattermole <acatterm@redhat.com>

--- pkg/extension/types/types.go
+++ pkg/extension/types/types.go
@@ -4,18 +4,21 @@ import (
 	"context"
 
 	celref "github.com/google/cel-go/common/types/ref"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	gatewayapiv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 )
 
 type Policy interface {
-	client.Object
+	GetName() string
+	GetNamespace() string
+	GetObjectKind() schema.ObjectKind
 	GetTargetRefs() []gatewayapiv1alpha2.LocalPolicyTargetReferenceWithSectionName
 }
 
 type KuadrantCtx interface {
 	Resolve(context.Context, Policy, string, bool) (celref.Val, error)
+	AddDataTo(context.Context, Policy, string, string) error
 }
 
 type ReconcileFn func(ctx context.Context, request reconcile.Request, kuadrant KuadrantCtx) (reconcile.Result, error)

cced39cc25d619250f50caf482094f4060691c50
dd di cesare <didi@posteo.net>
2025-06-30 14:56:25 +0200

    [feature] OIDCPolicy
    * API: Added as `v1alpha`
    * Refactor of `cmd/extension` to `cmd/extensions` in order to provide
      multiple
    
    Signed-off-by: dd di cesare <didi@posteo.net>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2025.
+=======
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package v1alpha1 contains API Schema definitions for the  v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=kuadrant.io
 package v1alpha1
@@ -24,9 +26,12 @@ import (
 )
 
 var (
+	// GroupVersion is group version used to register these objects
 	GroupVersion = schema.GroupVersion{Group: "kuadrant.io", Version: "v1alpha1"}
 
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
 
+	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )

70c2f61b635c65dbb022404e706e9f498cf0865a
Adam Cattermole <acatterm@redhat.com>
2025-08-26 11:54:01 +0100

    Trigger updates via Kuadrant resource
    Signed-off-by: Adam Cattermole <acatterm@redhat.com>

--- internal/extension/manager.go
+++ internal/extension/manager.go
@@ -32,7 +32,10 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/dynamic"
 
+	kuadrantv1beta1 "github.com/kuadrant/kuadrant-operator/api/v1beta1"
 	kuadrant "github.com/kuadrant/kuadrant-operator/pkg/cel/ext"
 	extpb "github.com/kuadrant/kuadrant-operator/pkg/extension/grpc/v1"
 )
@@ -47,6 +50,7 @@ type Manager struct {
 	dag        *nilGuardedPointer[StateAwareDAG]
 	logger     logr.Logger
 	sync       io.Writer
+	client     dynamic.Interface
 }
 
 type Extension interface {
@@ -55,7 +59,7 @@ type Extension interface {
 	Name() string
 }
 
-func NewManager(location string, logger logr.Logger, sync io.Writer) (Manager, error) {
+func NewManager(location string, logger logr.Logger, sync io.Writer, client dynamic.Interface) (Manager, error) {
 	names := discoverExtensions(logger, location)
 	if len(names) == 0 {
 		return Manager{}, ErrNoExtensionsFound
@@ -85,6 +89,7 @@ func NewManager(location string, logger logr.Logger, sync io.Writer) (Manager, e
 		BlockingDAG,
 		logger,
 		sync,
+		client,
 	}, err
 }
 
@@ -124,6 +129,53 @@ func (m *Manager) SetChangeNotifier(notifier ChangeNotifier) {
 	if service, ok := m.service.(*extensionService); ok {
 		service.changeNotifier = notifier
 	}
+}
+
+func (m *Manager) TriggerReconciliation(reason string) error {
+	logger := m.logger.WithName("TriggerReconciliation")
+	logger.V(1).Info("triggering reconciliation", "reason", reason)
+
+	kuadrantResource := m.client.Resource(kuadrantv1beta1.KuadrantsResource)
+	kuadrantList, err := kuadrantResource.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list Kuadrant resources: %w", err)
+	}
+	if len(kuadrantList.Items) == 0 {
+		return fmt.Errorf("no Kuadrant resources found in cluster")
+	}
+
+	for _, kuadrant := range kuadrantList.Items {
+		if kuadrant.GetDeletionTimestamp() != nil {
+			continue
+		}
+
+		annotations := kuadrant.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+		annotations["kuadrant.io/extension-trigger"] = time.Now().Format(time.RFC3339Nano)
+		annotations["kuadrant.io/extension-trigger-reason"] = reason
+		kuadrant.SetAnnotations(annotations)
+
+		_, err := kuadrantResource.Namespace(kuadrant.GetNamespace()).Update(
+			context.TODO(),
+			&kuadrant,
+			metav1.UpdateOptions{},
+		)
+		if err != nil {
+			logger.Error(err, "failed to update Kuadrant resource",
+				"namespace", kuadrant.GetNamespace(),
+				"name", kuadrant.GetName())
+			continue
+		}
+
+		logger.V(1).Info("successfully triggered reconciliation",
+			"namespace", kuadrant.GetNamespace(),
+			"name", kuadrant.GetName(),
+			"reason", reason)
+		return nil
+	}
+	return fmt.Errorf("failed to update any Kuadrant resource")
 }
 
 func (m *Manager) Run(stopCh <-chan struct{}) {

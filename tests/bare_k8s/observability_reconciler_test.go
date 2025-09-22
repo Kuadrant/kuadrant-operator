60419cfa29872a1272e4c0ce0080582961249140
Eguzki Astiz Lezaun <eastizle@redhat.com>
2025-09-22 15:24:35 +0200

    fix observability integration tests
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- tests/bare_k8s/observability_reconciler_test.go
+++ tests/bare_k8s/observability_reconciler_test.go
@@ -92,7 +92,7 @@ var _ = Describe("Observabiltity monitors for kuadrant components", func() {
 				},
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "kuadrant-limitador-monitor",
-					Namespace: kuadrantNamespace,
+					Namespace: testNamespace,
 				},
 			}
 

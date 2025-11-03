552d14cd3bd45e3021f2391450c9ae1bedc7130a
KevFan <chfan@redhat.com>
2025-11-03 12:54:52 +0000

    claude: /check-flaky-integration-tests for checking common flakey test patterns
    Signed-off-by: KevFan <chfan@redhat.com>

--- tests/istio/kuadrant_status_updater_reconciler_test.go
+++ tests/istio/kuadrant_status_updater_reconciler_test.go
@@ -117,25 +117,18 @@ var _ = Describe("kuadrant status reconciler", Serial, func() {
 
 	Context("when mTLS is on", func() {
 		BeforeEach(func(ctx SpecContext) {
-			kuadrantKey := client.ObjectKey{Name: "kuadrant-sample", Namespace: kuadrantInstallationNS}
-			patch := &kuadrantv1beta1.Kuadrant{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: kuadrantv1beta1.GroupVersion.String(),
-					Kind:       "Kuadrant",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      kuadrantKey.Name,
-					Namespace: kuadrantKey.Namespace,
-				},
-				Spec: kuadrantv1beta1.KuadrantSpec{
-					MTLS: &kuadrantv1beta1.MTLS{Enable: true},
-				},
-			}
-			Expect(testClient().Patch(ctx, patch, client.Apply, client.ForceOwnership, client.FieldOwner("test"))).To(Succeed())
+			kuadrantKey := client.ObjectKey{Name: tests.KuadrantName, Namespace: kuadrantInstallationNS}
+			Eventually(func(g Gomega) {
+				kuadrantCR := &kuadrantv1beta1.Kuadrant{}
+				g.Expect(testClient().Get(ctx, kuadrantKey, kuadrantCR)).To(Succeed())
+				patch := client.MergeFrom(kuadrantCR.DeepCopy())
+				kuadrantCR.Spec.MTLS = &kuadrantv1beta1.MTLS{Enable: true}
+				g.Expect(testClient().Patch(ctx, kuadrantCR, patch)).To(Succeed())
+			}).WithContext(ctx).Should(Succeed())
 		})
 
 		It("reconciles status mtls fields", func(ctx SpecContext) {
-			kuadrantKey := client.ObjectKey{Name: "kuadrant-sample", Namespace: kuadrantInstallationNS}
+			kuadrantKey := client.ObjectKey{Name: tests.KuadrantName, Namespace: kuadrantInstallationNS}
 			Eventually(func(g Gomega, ctx context.Context) {
 				kuadrantObj := &kuadrantv1beta1.Kuadrant{}
 				g.Expect(testClient().Get(ctx, kuadrantKey, kuadrantObj)).NotTo(HaveOccurred())
@@ -173,25 +166,18 @@ var _ = Describe("kuadrant status reconciler", Serial, func() {
 
 	Context("when mTLS is off", func() {
 		BeforeEach(func(ctx SpecContext) {
-			kuadrantKey := client.ObjectKey{Name: "kuadrant-sample", Namespace: kuadrantInstallationNS}
-			patch := &kuadrantv1beta1.Kuadrant{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: kuadrantv1beta1.GroupVersion.String(),
-					Kind:       "Kuadrant",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      kuadrantKey.Name,
-					Namespace: kuadrantKey.Namespace,
-				},
-				Spec: kuadrantv1beta1.KuadrantSpec{
-					MTLS: &kuadrantv1beta1.MTLS{Enable: false},
-				},
-			}
-			Expect(testClient().Patch(ctx, patch, client.Apply, client.ForceOwnership, client.FieldOwner("test"))).To(Succeed())
+			kuadrantKey := client.ObjectKey{Name: tests.KuadrantName, Namespace: kuadrantInstallationNS}
+			Eventually(func(g Gomega) {
+				kuadrantCR := &kuadrantv1beta1.Kuadrant{}
+				g.Expect(testClient().Get(ctx, kuadrantKey, kuadrantCR)).To(Succeed())
+				patch := client.MergeFrom(kuadrantCR.DeepCopy())
+				kuadrantCR.Spec.MTLS = &kuadrantv1beta1.MTLS{Enable: false}
+				g.Expect(testClient().Patch(ctx, kuadrantCR, patch)).To(Succeed())
+			}).WithContext(ctx).Should(Succeed())
 		})
 
 		It("reconciles status mtls fields", func(ctx SpecContext) {
-			kuadrantKey := client.ObjectKey{Name: "kuadrant-sample", Namespace: kuadrantInstallationNS}
+			kuadrantKey := client.ObjectKey{Name: tests.KuadrantName, Namespace: kuadrantInstallationNS}
 			Eventually(func(g Gomega, ctx context.Context) {
 				kuadrantObj := &kuadrantv1beta1.Kuadrant{}
 				g.Expect(testClient().Get(ctx, kuadrantKey, kuadrantObj)).NotTo(HaveOccurred())

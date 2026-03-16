bb987f41af8bec26fff2e8e50970cb371e6b6043
Adam Cattermole <a.d.cattermole@gmail.com>
2026-03-16 11:00:27 +0000

    Implementation for the ProtoCache
    Signed-off-by: Adam Cattermole <a.d.cattermole@gmail.com>

---
+++ internal/extension/proto_cache.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2026 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package extension
+
+import (
+	"sync"
+
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+type ProtoCacheKey struct {
+	ClusterName string
+	Service     string
+}
+
+type ProtoCache struct {
+	mu    sync.RWMutex
+	cache map[ProtoCacheKey]*descriptorpb.FileDescriptorSet
+}
+
+func NewProtoCache() *ProtoCache {
+	return &ProtoCache{
+		cache: make(map[ProtoCacheKey]*descriptorpb.FileDescriptorSet),
+	}
+}
+
+func (pc *ProtoCache) Set(key ProtoCacheKey, fds *descriptorpb.FileDescriptorSet) {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+	pc.cache[key] = fds
+}
+
+func (pc *ProtoCache) Get(key ProtoCacheKey) (*descriptorpb.FileDescriptorSet, bool) {
+	pc.mu.RLock()
+	defer pc.mu.RUnlock()
+	fds, exists := pc.cache[key]
+	return fds, exists
+}
+
+func (pc *ProtoCache) Delete(key ProtoCacheKey) bool {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+	_, existed := pc.cache[key]
+	if existed {
+		delete(pc.cache, key)
+	}
+	return existed
+}

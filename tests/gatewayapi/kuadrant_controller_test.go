749284f298a9e37d04fd53fb786b627879bbb17d
Jason Madigan <jason@jasonmadigan.com>
2026-09-15 15:20:40 +0100

    Fix Gateway API test cleanup timeout
    Signed-off-by: Jason Madigan <jason@jasonmadigan.com>

--- tests/gatewayapi/kuadrant_controller_test.go
+++ tests/gatewayapi/kuadrant_controller_test.go
@@ -24,13 +24,13 @@ var _ = Describe("Kuadrant controller when gateway provider is missing", func() 
 	var (
 		testNamespace string
 		// 20s to allow for slow CI runners where the operator may take longer to reconcile status
-		testTimeOut      = SpecTimeout(20 * time.Second)
+		testTimeOut      = NodeTimeout(20 * time.Second)
 		afterEachTimeOut = NodeTimeout(3 * time.Minute)
 	)
 
 	BeforeEach(func(ctx SpecContext) {
 		testNamespace = tests.CreateNamespace(ctx, testClient())
-	})
+	}, testTimeOut)
 
 	AfterEach(func(ctx SpecContext) {
 		tests.DeleteNamespace(ctx, testClient(), testNamespace)

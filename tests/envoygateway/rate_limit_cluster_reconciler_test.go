5606f744be90a3ace330191e970fdcfbab802e42
Thomas Maas <thomas@webtypes.com>
2026-09-17 13:40:09 +0200

    test: update envoygateway rate limit cluster assertion with new cluster fields
    The exact-match assertion on the EnvoyPatchPolicy cluster value was
    missing dns_refresh_rate, dns_lookup_family, and outlier_detection,
    added in buildClusterPatch by this PR.
    
    Signed-off-by: Thomas Maas <thomas@webtypes.com>

--- tests/envoygateway/rate_limit_cluster_reconciler_test.go
+++ tests/envoygateway/rate_limit_cluster_reconciler_test.go
@@ -168,9 +168,18 @@ var _ = Describe("limitador cluster controller", func() {
 
 			Expect(existingPatchValue).To(Equal(
 				map[string]any{
-					"name":                   kuadrant.KuadrantRateLimitClusterName,
-					"type":                   "STRICT_DNS",
-					"connect_timeout":        "1s",
+					"name":              kuadrant.KuadrantRateLimitClusterName,
+					"type":              "STRICT_DNS",
+					"connect_timeout":   "1s",
+					"dns_refresh_rate":  "5s",
+					"dns_lookup_family": "V4_ONLY",
+					"outlier_detection": map[string]any{
+						"consecutive_gateway_failure":           5,
+						"enforcing_consecutive_gateway_failure": 100,
+						"interval":                              "10s",
+						"base_ejection_time":                    "30s",
+						"max_ejection_percent":                  100,
+					},
 					"lb_policy":              "ROUND_ROBIN",
 					"http2_protocol_options": map[string]any{},
 					"load_assignment": map[string]any{

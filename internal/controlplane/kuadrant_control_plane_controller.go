6ad6926eb7bdb7f033c36c3221ba714716318dec
Michael Nairn <mnairn@redhat.com>
2026-09-24 04:36:13 -0700

    fix: log component installs and upgrades at info level
    Add logging for component installation and upgrade events alongside
    existing event recording. Installs and upgrades are significant
    one-time state changes that operators typically check logs for first,
    so they should be visible at Info level rather than only in Events.
    
    Signed-off-by: Michael Nairn <mnairn@redhat.com>

--- internal/controlplane/kuadrant_control_plane_controller.go
+++ internal/controlplane/kuadrant_control_plane_controller.go
@@ -282,8 +282,10 @@ func (r *Reconciler) emitComponentVersionEvents(cp *kuadrantv1alpha1.KuadrantCon
 		oldVersion, existed := previousVersions[cs.Name]
 		switch {
 		case !existed || oldVersion == "":
+			r.logger.Info("component installed", "component", cs.Name, "version", cs.ChartVersion)
 			r.recorder.Eventf(cp, componentReference(cs.Name), corev1.EventTypeNormal, "ComponentInstalled", "ComponentDeploy", "component %s installed at version %s", cs.Name, cs.ChartVersion)
 		case oldVersion != cs.ChartVersion:
+			r.logger.Info("component upgraded", "component", cs.Name, "from", oldVersion, "to", cs.ChartVersion)
 			r.recorder.Eventf(cp, componentReference(cs.Name), corev1.EventTypeNormal, "ComponentVersionChanged", "ComponentDeploy", "component %s updated from %s to %s", cs.Name, oldVersion, cs.ChartVersion)
 		}
 	}
